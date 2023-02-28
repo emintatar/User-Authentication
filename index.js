f2c21cc7dcd1e46fc646e58d4454dbae1ba50979
emintatar <emintatar00@gmail.com>
2023-02-28 18:00:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const hbs = require("hbs");
+const User = require("./mongodb");
+const port = 3000;
+const hostname = "127.0.0.1";
+
+app.set("view engine", "hbs");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const check = await User.findOne({
+      name: req.body.name,
+      password: req.body.password,
+    });
+    if (check) {
+      res.render("home");
+      console.log("User logged in");
+    } else {
+      res.redirect("/");
+      console.log("User not found");
+    }
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const user = {
+      name: req.body.name,
+      password: req.body.password,
+    };
+
+    const newUser = new User(user);
+    await newUser.save();
+    res.redirect("/login");
+    console.log("User created");
+  } catch (error) {
+    console.log(`Error: ${error}`);
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Example app listening at http://${hostname}:${port}`);
+});
---
+++ mongodb.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const hostname = "127.0.0.1";
+
+mongoose
+  .connect(`mongodb://${hostname}/sampledb`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+module.exports = User;
